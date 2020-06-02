6d712ec4456b0b63d4c8923d727054fb300d4ca6
hjsr <hendyjosue@akimad.com>
2020-06-02 17:22:45 +0200

    login init

---
+++ client/src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+	<Button
+		class="twitter-button"
+		type="primary"
+		icon="twitter"
+		@click="handleTwitter"
+	>
+		Login with Twitter
+	</Button>
+</template>
+
+<script>
+	import { Button } from 'ant-design-vue'
+	import { api } from '../utils/api'
+
+	export default {
+		name: 'Login',
+		components: { Button },
+		methods: {
+			handleTwitter() {
+				// api.get(`/connect/twitter?callback=${WEB_URL}/auth/twitter`)
+				api.get(`/connect/twitter`)
+			}
+		}
+	}
+</script>
+<style>
+	.twitter-button {
+		background-color: #1da1f2;
+	}
+</style>
