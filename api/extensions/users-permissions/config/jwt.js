1e559d5f06d1ed4795efe9841610e73976339ff4
hjsr <hendyjosue@akimad.com>
2020-06-02 17:22:58 +0200

    API updated by force AKA package.json rape

---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '162b030b-5384-4ea7-bc1c-9e00aa866bde'
+};
--- api/my-project/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'b1579b2a-6bc2-45fd-8f4e-69387c80e2f8'
-};
