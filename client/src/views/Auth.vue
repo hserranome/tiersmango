6b955e0414c4ad3223025888c073c63cace85df2
hjsr <hendyjosue@akimad.com>
2020-06-04 18:42:11 +0200

    Auth page UI

--- client/src/views/Auth.vue
+++ client/src/views/Auth.vue
@@ -1,31 +1,71 @@
 <template>
-	<p>Authenticating</p>
+	<layout class="auth-layout">
+		<Spin v-if="!authError" tip="Authenticating..." size="large" />
+		<Result
+			v-else
+			status="error"
+			title="Authentication failed"
+			sub-title="Please try again later."
+		>
+			<template #extra>
+				<router-link to="/">
+					<Button key="console" type="primary">
+						Go Home
+					</Button>
+				</router-link>
+				<router-link to="/login">
+					<Button key="buy">
+						Try again
+					</Button>
+				</router-link>
+			</template>
+		</Result>
+	</layout>
 </template>
 
 <script>
+	import Layout from '@/components/Layout'
+	import { Result, Spin, Button } from 'ant-design-vue'
 	import { api } from '../utils/api'
 
 	export default {
 		name: 'Auth',
+		components: { Layout, Result, Spin, Button },
+		data() {
+			return {
+				authError: false
+			}
+		},
 		created() {
 			this.handleAuth()
 		},
 		methods: {
 			handleAuth() {
-				console.log(this.$route)
 				const { provider } = this.$route.params
 				const { error } = this.$route.query
-				if (error) {
-					console.error(error)
-					return
+				if (!error) {
+					try {
+						api.get(`/auth/${provider}/callback`)
+						// Redirect
+					} catch (err) {
+						console.error(err)
+						this.authError = true
+					}
+				} else {
+					this.authError = true
 				}
-				const data = api.get(`/auth/${provider}/callback`)
-				console.log(data)
 			}
 		}
 	}
 </script>
 <style>
+	.auth-layout {
+		padding-top: 80px !important;
+		min-height: 60vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 	.twitter-button {
 		background-color: #1da1f2;
 	}
