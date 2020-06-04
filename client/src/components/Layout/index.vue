8f69a4d26b7034395e43e8217406a8cca5715a8a
hjsr <hendyjosue@akimad.com>
2020-06-04 02:28:17 +0200

    layout component

---
+++ client/src/components/Layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="main-content">
+		<slot />
+	</div>
+</template>
+<script>
+	export default {
+		name: 'Layout'
+	}
+</script>
+<style lang="scss" scoped>
+	.main-content {
+		max-width: 1440px;
+		padding: 0 20px;
+		margin: 0 auto;
+	}
+</style>
